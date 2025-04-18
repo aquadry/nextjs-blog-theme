32ef971fcae0b34a29c5a0561cde298f0236b938
aquadry <aquadry@outlook.com>
2025-04-18 23:47:15 +0200

    yyy

--- components/Footer.js
+++ components/Footer.js
@@ -81,7 +81,7 @@ export default function Footer({ copyrightText, year}) {
   return (
     <footer className="flex flex-col items-center py-16">
       <p className="mb-3 font-bold uppercase dark:text-white opacity-60">
-        {copyrightText + "&nbs&nbs&nbs&nbs" + year}
+        {copyrightText} + "&nbs&nbs&nbs&nbs" + {year}
       </p>
       <ThemeSwitcher />
     </footer>
