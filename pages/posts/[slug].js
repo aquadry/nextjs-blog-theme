eee2eec717cd360bac6938f39087f1ff79f96876
aquadry <aquadry@outlook.com>
2025-04-19 13:08:59 +0200

    xxx

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -98,7 +98,7 @@ export default function PostPage({
           )}
         </div>
       </article>
-      <Footer copyrightText={globalData.footerText} />
+      <Footer copyrightText={globalData.footerText} year={globalData.year}/>
       <GradientBackground
         variant="large"
         className="absolute -top-32 opacity-30 dark:opacity-50"
